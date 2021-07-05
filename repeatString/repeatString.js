115a30122c654652fef736f575da7bb4c30b0161
Ryan <coulson.ryan@gmail.com>
2021-07-05 17:03:03 +0200

    first commit

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (x, y) {
+  let string = '';
+  if (y < 0) {
+    return 'ERROR';
+  } else {
+    for (let i = 0; i < y; i++) {
+      string += x;
+    }
+    return string;
+  }
 };
 
 module.exports = repeatString;
